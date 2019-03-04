2999a3825fc2754e976bbfd504c12d1789240429
angular-moon <xiaoxuss@163.com>
2019-03-04 15:31:05 +0800

    默认生成registry相关api, CloudComponent 支持通过名字加载组件
    CloudComponent 增加导出 loadComponent
    fix bug:
    之前的版本 prop: url 不会传递给加载的组件,
    如果组件本身存在 prop: url 则不能正常工作
    现在你可以通过loadComponent加载组件

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,21 @@ import { Router } from 'react-router-dom';
 import WhatToEat from './components/WhatToEat';
 import { Mode } from './enums/Mode';
 import { stateContainer } from './utils';
+import CloudComponent, { loadComponent } from '@/components/CloudComponent';
+
+// 通过组件名字 从 registry.gmsoftdev.com 加载
+const CloudWhatToEat = loadComponent({ name: 'test-project/WhatToEat' });
 
 const App = () => (
   <LocaleProvider locale={zhCN}>
     <Provider store={stateContainer._store}>
       <Router history={stateContainer._history}>
-        <WhatToEat defaultMode={Mode.DRAW} />
+        <>
+          <WhatToEat defaultMode={Mode.DRAW} />
+          <CloudWhatToEat />
+          {/* 通过组件名字 从 registry.gmsoftdev.com 加载 */}
+          <CloudComponent name="test-project/WhatToEat" defaultMode="search" />
+        </>
       </Router>
     </Provider>
   </LocaleProvider>
