0cfc89e7ed88720370b5f5bcabf9d7914be5d996
angular-moon <xiaoxuss@163.com>
2019-04-28 14:12:18 +0800

    update antd@3.16.6-custom.0, remove use LocaleProvider
    antd定制版本已经替换默认语言为中文

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { LocaleProvider } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
 import React from 'react';
 import { hot, setConfig } from 'react-hot-loader';
 import { Provider } from 'react-redux';
@@ -13,18 +11,16 @@ import CloudComponent, { loadComponent } from '@/components/CloudComponent';
 const CloudWhatToEat = loadComponent({ name: 'test-project/WhatToEat' });
 
 const App = () => (
-  <LocaleProvider locale={zhCN}>
-    <Provider store={stateContainer._store}>
-      <Router history={stateContainer._history}>
+  <Provider store={stateContainer._store}>
+    <Router history={stateContainer._history}>
         <>
           <WhatToEat defaultMode={Mode.DRAW} />
           <CloudWhatToEat />
           {/* 通过组件名字 从 registry.gmsoftdev.com 加载 */}
           <CloudComponent name="test-project/WhatToEat" defaultMode="search" />
         </>
-      </Router>
-    </Provider>
-  </LocaleProvider>
+    </Router>
+  </Provider>
 );
 
 // @ts-ignore
