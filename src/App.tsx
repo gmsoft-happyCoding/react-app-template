4e3084d96cc6b311e45a20fd2b6f82402ac804ab
angular-moon <xiaoxuss@163.com>
2019-06-19 11:33:55 +0800

    update react-hot-loader@4.11.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { hot, setConfig } from 'react-hot-loader';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import WhatToEat from './components/WhatToEat';
@@ -13,16 +13,14 @@ const CloudWhatToEat = loadComponent({ name: 'test-project/WhatToEat' });
 const App = () => (
   <Provider store={stateContainer._store}>
     <Router history={stateContainer._history}>
-        <>
-          <WhatToEat defaultMode={Mode.DRAW} />
-          <CloudWhatToEat />
-          {/* 通过组件名字 从 registry.gmsoftdev.com 加载 */}
-          <CloudComponent name="test-project/WhatToEat" defaultMode="search" />
-        </>
+      <>
+        <WhatToEat defaultMode={Mode.DRAW} />
+        <CloudWhatToEat />
+        {/* 通过组件名字 从 registry.gmsoftdev.com 加载 */}
+        <CloudComponent name="test-project/WhatToEat" defaultMode="search" />
+      </>
     </Router>
   </Provider>
 );
 
-// @ts-ignore
-setConfig({ pureSFC: true });
-export default hot(module)(App);
+export default hot(App);
