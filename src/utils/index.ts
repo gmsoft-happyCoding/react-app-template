0bc1ad4b768433bf7751b69b5bc8f0179014c2e7
angular-moon <xiaoxuss@163.com>
2019-06-20 15:51:33 +0800

    remove deprecated tools

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,9 @@
 import stateContainer from './stateContainer';
-import bindActions from './bindActions';
-import unwrapActions from './unwrapActions';
 import { setConfirm, resetConfirm } from './confirmation';
 import * as notification from './notification';
 
 export {
   stateContainer,
-  bindActions,
-  unwrapActions,
   setConfirm as setRouterConfirm,
   resetConfirm as resetRouterConfirm,
   notification,
